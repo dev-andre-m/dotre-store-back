0759a693b71265bed14162ec54a16e0de19ebf91
Andre Mendes <dev.andre.m@icloud.com>
2025-09-15 14:22:42 +0200

    added CORS configuration, removed unused code/import

--- DotreApi/Program.cs
+++ DotreApi/Program.cs
@@ -1,10 +1,7 @@
-using System.Collections.Immutable;
 using DotreApi.Data;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
 
 builder.Services.AddControllers();
 builder.Services.AddDbContext<StoreContext>(opt =>
@@ -13,20 +10,14 @@ builder.Services.AddDbContext<StoreContext>(opt =>
 
 });
 
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddCors();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseCors(opt =>
 {
-    app.MapOpenApi();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
+    opt.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:3000");
+});
 
 app.MapControllers();
 
